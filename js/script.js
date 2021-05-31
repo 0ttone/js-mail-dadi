03e55d9b0f6a2c0ff4f52791f6e709166256e33b
MyUsername <emanuele.marini37@gmail.com>
2021-05-31 15:50:20 +0200

    prima stesura

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/* creare var mailUser da prompt e subito inserire in html
+Successivamente preparare array con 5 mail con scrittura estesa
+senza ridurre e confrontare il dato - creare campo in html a prescindere dal 
+check con console.log - per ora rendere tutto visibile in pagina
+che sia valida o meno
